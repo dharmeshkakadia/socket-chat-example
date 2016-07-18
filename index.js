63cdb3f811347cf794e77f6227f9f4bad651cd2f
Dharmesh Kakadia <dharmesh.kakadia@research.iiit.ac.in>
2016-07-17 20:48:08 -0700

    HW3

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Set = require("collections/set");
+var typingusers = new Set();
 
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/index.html');
@@ -8,14 +10,25 @@ app.get('/', function(req,res){
 
 io.on('connection', function(socket){
 	var user = 'unknown';
+
 	socket.on('chat message', function(msg){
 		socket.broadcast.emit('chat message', msg);
+	});
+	socket.on('typing', function(u){
+		typingusers.add(u);
+		socket.broadcast.emit('typing', typingusers);
+	});
+	socket.on('nottyping', function(u){
+		typingusers.delete(u);
+		socket.broadcast.emit('nottyping', typingusers);
 	});
 	socket.on('user connected', function(u){
 		user=u;
 		socket.broadcast.emit('chat message', 'user '+user+' joined');
 	});
 	socket.on('disconnect', function(){
+		typingusers.delete(user)
+		socket.emit('nottyping', user);
 		socket.broadcast.emit('chat message', 'user '+user+' left');
 	});
 });
